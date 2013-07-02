528500dd423ce51951a0279f0816216b49c6eb58
Julien Lancelot <julien.lancelot@gmail.com>
2013-07-02 10:00:05 +0200

    Reactivate previously commented tests

--- it-sonar/tests/src/test/java/com/sonar/it/administration/suite/AdministrationTestSuite.java
+++ it-sonar/tests/src/test/java/com/sonar/it/administration/suite/AdministrationTestSuite.java
@@ -13,15 +13,15 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-//  BackupTest.class,
-//  BulkDeletionTest.class,
-//  DashboardSharingPermissionTest.class,
+  BackupTest.class,
+  BulkDeletionTest.class,
+  DashboardSharingPermissionTest.class,
   PermissionsTest.class,
-//  ProjectAdministrationTest.class,
-//  PropertySetsTest.class,
-//  SubCategoriesTest.class,
+  ProjectAdministrationTest.class,
+  PropertySetsTest.class,
+  SubCategoriesTest.class,
   SystemAdminPermissionTest.class,
-//  UserAdministrationTest.class
+  UserAdministrationTest.class
 })
 public class AdministrationTestSuite {
 
