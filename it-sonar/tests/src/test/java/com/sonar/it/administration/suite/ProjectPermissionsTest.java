d735344d49b755b13895e26c8447ad8a4e7be517
Jean-Baptiste Vilain <jean-baptiste.vilain@sonarsource.com>
2013-07-18 18:27:08 +0200

    SONAR-4454 Added test to application of a permission template to a single project

--- it-sonar/tests/src/test/java/com/sonar/it/administration/suite/ProjectPermissionsTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/administration/suite/ProjectPermissionsTest.java
@@ -10,6 +10,8 @@ import com.sonar.it.ItUtils;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.orchestrator.selenium.Selenese;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -18,33 +20,60 @@ public class ProjectPermissionsTest {
   @ClassRule
   public static Orchestrator orchestrator = AdministrationTestSuite.ORCHESTRATOR;
 
+  @BeforeClass
+  public static void setUpProjects() {
+    // To be replaced by the corresponding ws call to create the test template
+    Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("create-permission-template-to-apply",
+      "/selenium/administration/project-permissions/create-permission-template.html")
+      .build();
+    orchestrator.executeSelenese(selenese);
+  }
+
+  @Before
+  public void cleanUpAnalysisData() {
+    orchestrator.getDatabase().truncateInspectionTables();
+  }
+
   /**
    * SONAR-4453
    */
   @Test
   public void should_bulk_apply_permission_template() throws Exception {
 
-    Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("create-permission-template-to-apply",
-      "/selenium/administration/project-permissions/create-permission-template.html")
-      .build();
-    orchestrator.executeSelenese(selenese);
-
-    orchestrator.getDatabase().truncateInspectionTables();
     SonarRunner sampleProject = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample"))
       .setProperties("sonar.dynamicAnalysis", "false")
       .setRunnerVersion("2.2.2");
     orchestrator.executeBuild(sampleProject);
+
     SonarRunner sampleProjectWithTests = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample-with-tests"))
       .setProperties("sonar.dynamicAnalysis", "false")
       .setRunnerVersion("2.2.2");
     orchestrator.executeBuild(sampleProjectWithTests);
+
     SonarRunner sampleMultiModulesProject = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-multi-modules-sample"))
       .setProperties("sonar.dynamicAnalysis", "false")
       .setRunnerVersion("2.2.2");
     orchestrator.executeBuild(sampleMultiModulesProject);
 
-    selenese = Selenese.builder().setHtmlTestsInClasspath("bulk-apply-permission-template",
+    Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("bulk-apply-permission-template",
       "/selenium/administration/project-permissions/bulk-apply-permission-template.html")
+      .build();
+    orchestrator.executeSelenese(selenese);
+  }
+
+  /**
+   * SONAR-4454
+   */
+  @Test
+  public void should_apply_template_to_single_project() throws Exception {
+
+    SonarRunner sampleProject = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample"))
+      .setProperties("sonar.dynamicAnalysis", "false")
+      .setRunnerVersion("2.2.2");
+    orchestrator.executeBuild(sampleProject);
+
+    Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("apply-permission-template-to-project",
+      "/selenium/administration/project-permissions/apply-permission-template-to-project.html")
       .build();
     orchestrator.executeSelenese(selenese);
   }
