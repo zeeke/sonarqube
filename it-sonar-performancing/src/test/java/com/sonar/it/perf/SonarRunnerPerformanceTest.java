f0ea85b0d8c21fedeadb9fc055a4cf889476fe23
Julien HENRY <julien.henry@sonarsource.com>
2013-07-22 14:56:18 +0200

    Add performance IT for dry run

--- it-sonar-performancing/src/test/java/com/sonar/it/perf/SonarRunnerPerformanceTest.java
+++ it-sonar-performancing/src/test/java/com/sonar/it/perf/SonarRunnerPerformanceTest.java
@@ -9,7 +9,12 @@ import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.MavenBuild;
 import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.orchestrator.locator.FileLocation;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,8 +28,8 @@ public class SonarRunnerPerformanceTest {
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
-    .restoreProfileAtStartup(FileLocation.ofClasspath("/sonar-way-3.6.xml"))
-    .build();
+      .restoreProfileAtStartup(FileLocation.ofClasspath("/sonar-way-3.6.xml"))
+      .build();
 
   @Rule
   public TestName testName = new TestName();
@@ -33,8 +38,8 @@ public class SonarRunnerPerformanceTest {
   public static void setUp() {
     // Execute sonar to generate jacoco reports
     MavenBuild build = MavenBuild.create(FileLocation.ofShared("it-sonar-performancing/tika-1.3/pom.xml").getFile())
-      .setEnvironmentVariable("MAVEN_OPTS", "-Xmx512m -server")
-      .setGoals("clean test-compile");
+        .setEnvironmentVariable("MAVEN_OPTS", "-Xmx512m -server")
+        .setGoals("clean test-compile");
     orchestrator.executeBuild(build);
 
     // Set coverage per test profile as goal, waiting for orchestrator API to set maven profile
@@ -56,10 +61,10 @@ public class SonarRunnerPerformanceTest {
   @Test
   public void most_basic_scan() {
     SonarRunner runner = newSonarRunner("-Xmx512m -server",
-      "sonar.dynamicAnalysis", "false",
-      "sonar.profile", "empty",
-      "sonar.cpd.skip", "true"
-    );
+        "sonar.dynamicAnalysis", "false",
+        "sonar.profile", "empty",
+        "sonar.cpd.skip", "true"
+        );
     long start = System.currentTimeMillis();
     orchestrator.executeBuild(runner);
     long duration = System.currentTimeMillis() - start;
@@ -70,9 +75,9 @@ public class SonarRunnerPerformanceTest {
   public void scan_with_java_rules() {
     // checkstyle, pmd and squid but not findbugs
     SonarRunner runner = newSonarRunner("-Xmx512m -server",
-      "sonar.dynamicAnalysis", "false",
-      "sonar.profile", "sonar-way"
-    );
+        "sonar.dynamicAnalysis", "false",
+        "sonar.profile", "sonar-way"
+        );
     long start = System.currentTimeMillis();
     orchestrator.executeBuild(runner);
     long duration = System.currentTimeMillis() - start;
@@ -80,16 +85,30 @@ public class SonarRunnerPerformanceTest {
     assertDuration(duration, 120000L);
   }
 
+  @Test
+  public void dry_run_scan_with_java_rules() {
+    // checkstyle, pmd and squid but not findbugs
+    SonarRunner runner = newSonarRunner("-Xmx512m -server",
+        "sonar.dynamicAnalysis", "false",
+        "sonar.profile", "sonar-way",
+        "sonar.dryRun", "true"
+        );
+    long start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    long duration = System.currentTimeMillis() - start;
+
+    assertDuration(duration, 120000L);
+  }
 
   @Test
   @Ignore("To be enabled when most_basic_scan passes")
   public void scan_with_no_rule_and_coverage_per_test() {
     SonarRunner runner = newSonarRunner("-Xmx512m -server",
-      "sonar.dynamicAnalysis", "reuseReports",
-      "sonar.surefire.reportsPath", "target/surefire-reports",
-      "sonar.jacoco.reportPath", "target/jacoco.exec",
-      "sonar.profile", "empty"
-    );
+        "sonar.dynamicAnalysis", "reuseReports",
+        "sonar.surefire.reportsPath", "target/surefire-reports",
+        "sonar.jacoco.reportPath", "target/jacoco.exec",
+        "sonar.profile", "empty"
+        );
     long start = System.currentTimeMillis();
     orchestrator.executeBuild(runner);
     long duration = System.currentTimeMillis() - start;
@@ -99,21 +118,21 @@ public class SonarRunnerPerformanceTest {
   @Test
   public void should_not_fail_with_limited_xmx_memory_and_no_coverage_per_test() {
     orchestrator.executeBuild(
-      newSonarRunner("-Xmx80m -server -XX:-HeapDumpOnOutOfMemoryError", "sonar.dynamicAnalysis", "false")
-    );
+        newSonarRunner("-Xmx80m -server -XX:-HeapDumpOnOutOfMemoryError", "sonar.dynamicAnalysis", "false")
+        );
   }
 
-  private static SonarRunner newSonarRunner(String sonarRunnerOpts, String... props){
+  private static SonarRunner newSonarRunner(String sonarRunnerOpts, String... props) {
     return SonarRunner.create()
-      .setProperties(props)
-      .setEnvironmentVariable("SONAR_RUNNER_OPTS", sonarRunnerOpts)
-      .setRunnerVersion("2.2.2")
-      .setProjectDir(FileLocation.ofShared("it-sonar-performancing/tika-1.3").getFile());
+        .setProperties(props)
+        .setEnvironmentVariable("SONAR_RUNNER_OPTS", sonarRunnerOpts)
+        .setRunnerVersion("2.2.2")
+        .setProjectDir(FileLocation.ofShared("it-sonar-performancing/tika-1.3").getFile());
   }
 
   private void assertDuration(long duration, long expectedDuration) {
     double variation = 100.0 * (0.0 + duration - expectedDuration) / expectedDuration;
-    LOG.info("Test '" + testName.getMethodName() +" ' executed in " + duration + " ms (" + variation + "% from target)");
+    LOG.info("Test '" + testName.getMethodName() + " ' executed in " + duration + " ms (" + variation + "% from target)");
     assertThat(Math.abs(variation)).isLessThan(ACCEPTED_DURATION_VARIATION_IN_PERCENTS);
   }
 
