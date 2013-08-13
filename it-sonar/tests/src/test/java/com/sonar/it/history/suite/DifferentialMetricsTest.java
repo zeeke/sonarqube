92b4d4968243451ce2264cc63a3c6f1bb026584c
Jean-Baptiste Vilain <jean-baptiste.vilain@sonarsource.com>
2013-08-13 16:18:02 +0200

    SONAR-3858 Added ITs covering the differential metrics drilldown

---
+++ it-sonar/tests/src/test/java/com/sonar/it/history/suite/DifferentialMetricsTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2009-2012 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+
+package com.sonar.it.history.suite;
+
+import com.sonar.it.ItUtils;
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarRunner;
+import com.sonar.orchestrator.locator.FileLocation;
+import com.sonar.orchestrator.selenium.Selenese;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class DifferentialMetricsTest {
+
+  @ClassRule
+  public static Orchestrator orchestrator = Orchestrator.builderEnv()
+    .addPlugin(ItUtils.xooPlugin())
+    .restoreProfileAtStartup(FileLocation.ofClasspath("/com/sonar/it/history/DifferentialMetricsTest/one-issue-per-line-profile.xml"))
+    .build();
+
+  @BeforeClass
+  public static void scanProject() {
+    SonarRunner firstBuild = SonarRunner
+      .create(ItUtils.locateProjectDir("shared/xoo-history-v1"), "sonar.profile", "one-issue-per-line-profile", "sonar.projectDate", "2013-01-05");
+    SonarRunner secondBuild = SonarRunner
+      .create(ItUtils.locateProjectDir("shared/xoo-history-v2"), "sonar.profile", "one-issue-per-line-profile", "sonar.projectDate", "2013-01-07");
+    orchestrator.executeBuild(firstBuild);
+    orchestrator.executeBuild(secondBuild);
+  }
+
+  /**
+   * SONAR-3858
+   */
+  @Test
+  public void should_display_modified_files_in_differential_drilldown() throws Exception {
+    Selenese selenese = Selenese.builder()
+      .setHtmlTestsInClasspath("differential-drilldown",
+        "/selenium/history/differential-metrics/display-added-files.html",
+        "/selenium/history/differential-metrics/display-modified-files.html"
+      ).build();
+    orchestrator.executeSelenese(selenese);
+  }
+}
