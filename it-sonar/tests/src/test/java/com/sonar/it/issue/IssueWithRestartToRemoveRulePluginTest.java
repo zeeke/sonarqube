3044d88f442e94cebd679f8a4b694fda0f0a2a9f
Julien Lancelot <julien.lancelot@gmail.com>
2013-08-23 10:04:00 +0200

    SONAR-4559 Add IT

--- it-sonar/tests/src/test/java/com/sonar/it/issue/IssueWithRestartToRemoveRulePluginTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/issue/IssueWithRestartToRemoveRulePluginTest.java
@@ -10,11 +10,15 @@ import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.orchestrator.locator.FileLocation;
 import com.sonar.orchestrator.selenium.Selenese;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.sonar.wsclient.issue.Issue;
 import org.sonar.wsclient.issue.IssueClient;
 import org.sonar.wsclient.issue.IssueQuery;
+
+import java.io.File;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -25,6 +29,11 @@ public class IssueWithRestartToRemoveRulePluginTest {
     .addPlugin(ItUtils.xooPlugin())
     .addPlugin(ItUtils.locateTestPlugin("deprecated-xoo-rule-plugin"))
     .build();
+
+  @Before
+  public void resetData() {
+    orchestrator.getDatabase().truncateInspectionTables();
+  }
 
   /**
    * SONAR-4364
@@ -56,6 +65,35 @@ public class IssueWithRestartToRemoveRulePluginTest {
     issue = issueClient.find(IssueQuery.create().rules("deprecated-repo:deprecated-rule")).list().get(0);
     assertThat(issue.status()).isEqualTo("CLOSED");
     assertThat(issue.resolution()).isEqualTo("REMOVED");
+  }
+
+  /**
+   * SONAR-4559
+   */
+  @Test
+  public void issues_on_removed_rule_should_be_displayed_in_issues_code_viewer() throws Exception {
+    orchestrator.getDatabase().truncateInspectionTables();
+    orchestrator.getServer().restoreProfile(FileLocation.ofClasspath("/com/sonar/it/issue/suite/with-deprecated-rule-profile.xml"));
+    SonarRunner scan = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample"))
+      .setProfile("with-deprecated-rule")
+      .setRunnerVersion("2.2.2");
+    orchestrator.executeBuild(scan);
+
+    // Remove deprecated rule plugin with updatecenter web console because there's no way to do that with the Orchestrator API.
+    orchestrator.executeSelenese(
+      Selenese.builder().setHtmlTestsInClasspath("remove-rule-plugin", "/selenium/issue/remove-deprecated-rule-plugin.html").build()
+    );
+    orchestrator.restartSonar();
+
+    orchestrator.executeSelenese(
+      Selenese.builder().setHtmlTestsInClasspath("display-issues-code-viewer-on-removed-rule",
+        // SONAR-4559
+        "/selenium/issue/display-issues-code-viewer-on-removed-rule.html"
+      ).build()
+    );
+
+    File logs = orchestrator.getServer().getLogs();
+    assertThat(FileUtils.readFileToString(logs)).doesNotContain("nil:NilClass");
   }
 
 }
