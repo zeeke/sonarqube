e38c1c3f9e0fdce6794d4dcf96748fc2731c6f96
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2013-09-09 18:09:09 +0200

    SONAR-3644 Add tests for regexp-based features
    Add marker regexp to test project
    Add test for block exclusion
    Add test for whole file exclusion
    Add test for empty exclusion configuration (all issues reported)

--- it-sonar/tests/src/test/java/com/sonar/it/issue2/suite/IssueExclusionsTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/issue2/suite/IssueExclusionsTest.java
@@ -26,6 +26,20 @@ public class IssueExclusionsTest extends AbstractIssueTestCase2 {
   }
 
   @Test
+  public void should_not_exclude_anything() {
+    scan();
+
+    Resource project = orchestrator.getServer().getWsClient().find(ResourceQuery.createForMetrics(PROJECT_KEY, "violations", "info_violations", "minor_violations", "major_violations",
+      "blocker_violations", "critical_violations"));
+    assertThat(project.getMeasureIntValue("violations")).isEqualTo(70);
+    assertThat(project.getMeasureIntValue("info_violations")).isEqualTo(2);     // Has tag 'xoo'
+    assertThat(project.getMeasureIntValue("minor_violations")).isEqualTo(57);   // One per line
+    assertThat(project.getMeasureIntValue("major_violations")).isEqualTo(4);    // One per file
+    assertThat(project.getMeasureIntValue("blocker_violations")).isEqualTo(0);
+    assertThat(project.getMeasureIntValue("critical_violations")).isEqualTo(7); // One per module
+  }
+
+  @Test
   public void should_ignore_all_files() {
     scan(
       "sonar.issue.ignore.multicriteria", "1",
@@ -34,14 +48,48 @@ public class IssueExclusionsTest extends AbstractIssueTestCase2 {
       "sonar.issue.ignore.multicriteria.1.lineRange", "*"
       );
 
-    assertThat(searchIssuesByComponent(PROJECT_KEY)).hasSize(7);
-
     Resource project = orchestrator.getServer().getWsClient().find(ResourceQuery.createForMetrics(PROJECT_KEY, "violations", "info_violations", "minor_violations", "major_violations",
-      "blocker_violations", "critical_violations", "violations_density"));
+      "blocker_violations", "critical_violations"));
     assertThat(project.getMeasureIntValue("violations")).isEqualTo(7);
     assertThat(project.getMeasureIntValue("info_violations")).isEqualTo(0);
     assertThat(project.getMeasureIntValue("minor_violations")).isEqualTo(0);
     assertThat(project.getMeasureIntValue("major_violations")).isEqualTo(0);
+    assertThat(project.getMeasureIntValue("blocker_violations")).isEqualTo(0);
+    assertThat(project.getMeasureIntValue("critical_violations")).isEqualTo(7);
+  }
+
+  @Test
+  public void should_ignore_files_with_regexp() {
+    scan(
+      "sonar.issue.ignore.allfile", "1",
+      "sonar.issue.ignore.allfile.1.fileRegexp", "EXTERMINATE-ALL-ISSUES"
+      );
+
+    Resource project = orchestrator.getServer().getWsClient().find(ResourceQuery.createForMetrics(PROJECT_KEY, "violations", "info_violations", "minor_violations", "major_violations",
+      "blocker_violations", "critical_violations"));
+    assertThat(project.getMeasureIntValue("violations")).isEqualTo(70 - 1 /* tag */ - 18 /* lines in HelloA1.xoo */ - 1 /* file */);
+    assertThat(project.getMeasureIntValue("info_violations")).isEqualTo(2 - 1);
+    assertThat(project.getMeasureIntValue("minor_violations")).isEqualTo(57 - 18);
+    assertThat(project.getMeasureIntValue("major_violations")).isEqualTo(4 - 1);
+    assertThat(project.getMeasureIntValue("blocker_violations")).isEqualTo(0);
+    assertThat(project.getMeasureIntValue("critical_violations")).isEqualTo(7);
+  }
+
+  @Test
+  public void should_ignore_block_with_regexp() {
+    scan(
+      "sonar.issue.ignore.block", "1",
+      "sonar.issue.ignore.block.1.beginBlockRegexp", "MUTE-SONAR",
+      "sonar.issue.ignore.block.1.endBlockRegexp", "UNMUTE-SONAR",
+      "sonar.verbose", "true"
+      );
+
+    Resource project = orchestrator.getServer().getWsClient().find(ResourceQuery.createForMetrics(PROJECT_KEY, "violations", "info_violations", "minor_violations", "major_violations",
+      "blocker_violations", "critical_violations"));
+//    assertThat(project.getMeasureIntValue("violations")).isEqualTo(70 - 1 /* tag */ - 5 /* lines in HelloA2.xoo */);
+    assertThat(project.getMeasureIntValue("info_violations")).isEqualTo(2 - 1);
+    assertThat(project.getMeasureIntValue("minor_violations")).isEqualTo(57 - 5);
+    assertThat(project.getMeasureIntValue("major_violations")).isEqualTo(4);
     assertThat(project.getMeasureIntValue("blocker_violations")).isEqualTo(0);
     assertThat(project.getMeasureIntValue("critical_violations")).isEqualTo(7);
   }
