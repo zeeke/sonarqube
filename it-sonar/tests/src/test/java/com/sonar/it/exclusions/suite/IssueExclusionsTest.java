084e46a718382a70006bd2737d7d4927ac807230
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2013-09-10 11:31:42 +0200

    SONAR-3644 Fix missing license header

--- it-sonar/tests/src/test/java/com/sonar/it/exclusions/suite/IssueExclusionsTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/exclusions/suite/IssueExclusionsTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2009-2012 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
 package com.sonar.it.exclusions.suite;
 
 import com.sonar.it.issue2.suite.AbstractIssueTestCase2;
