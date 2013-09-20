fdff53b1a903428e52058f74cd3aaf8f14123947
Julien Lancelot <julien.lancelot@gmail.com>
2013-09-20 15:17:46 +0200

    SONAR-4694 Use new component resource WS on issue permission test

--- it-sonar/tests/src/test/java/com/sonar/it/issue2/suite/IssuePermissionTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/issue2/suite/IssuePermissionTest.java
@@ -14,6 +14,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.sonar.wsclient.SonarClient;
+import org.sonar.wsclient.permissions.PermissionParameters;
 import org.sonar.wsclient.user.UserParameters;
 
 public class IssuePermissionTest extends AbstractIssueTestCase2 {
@@ -37,27 +38,20 @@ public class IssuePermissionTest extends AbstractIssueTestCase2 {
   public void need_code_viewer_permission_to_see_source_code_from_issue() {
     orchestrator.getServer().restoreProfile(FileLocation.ofClasspath("/com/sonar/it/issue/suite/one-issue-per-line-profile.xml"));
     SonarRunner sampleProject = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample"))
-      .setProperties("sonar.dynamicAnalysis", "false",
-        // Change project key and name because project permissions cannot be rest by WS -> Use unique project key / name to have no conflict with other tests
-        "sonar.projectKey", "sample-issue-code-viewer-permission", "sonar.projectName", "sample-issue-code-viewer-permission")
+      .setProperties("sonar.dynamicAnalysis", "false")
       .setRunnerVersion("2.2.2")
       .setProfile("one-issue-per-line");
     orchestrator.executeBuild(sampleProject);
 
     SonarClient client = ItUtils.newWsClientForAdmin(orchestrator);
-    UserParameters newUser = UserParameters.create().login(WITH_CODE_VIEWER_PERMISSION).name(WITH_CODE_VIEWER_PERMISSION)
-      .password("password").passwordConfirmation("password");
-    client.userClient().create(newUser);
-    newUser = UserParameters.create().login(WITHOUT_CODE_VIEWER_PERMISSION).name(WITHOUT_CODE_VIEWER_PERMISSION)
-      .password("password").passwordConfirmation("password");
-    client.userClient().create(newUser);
+    client.userClient().create(UserParameters.create().login(WITH_CODE_VIEWER_PERMISSION).name(WITH_CODE_VIEWER_PERMISSION)
+      .password("password").passwordConfirmation("password"));
+    client.permissionClient().addPermission(PermissionParameters.create().user(WITH_CODE_VIEWER_PERMISSION).component("sample").permission("codeviewer"));
 
-    // Use Selenium to grant permissions to user as there's currently no WS to do that : remove all users/groups code viewer permission on project and the permission to one user
-    orchestrator.executeSelenese(Selenese.builder().setHtmlTestsInClasspath("initialize-code-viewer-permissions-on-project",
-      "/selenium/issue/issue-permissions/initialize-code-viewer-permissions-on-project.html")
-      .build());
+    client.userClient().create(UserParameters.create().login(WITHOUT_CODE_VIEWER_PERMISSION).name(WITHOUT_CODE_VIEWER_PERMISSION)
+      .password("password").passwordConfirmation("password"));
+    client.permissionClient().removePermission(PermissionParameters.create().group("anyone").component("sample").permission("codeviewer"));
 
-    // Now the real test begins!
     orchestrator.executeSelenese(Selenese.builder().setHtmlTestsInClasspath("need-code-viewer-permission-to-see-source-code-from-issue",
       "/selenium/issue/issue-permissions/without-code-viewer-permission-source-code-from-issue-is-hidden.html",
       "/selenium/issue/issue-permissions/with-code-viewer-permission-source-code-from-issue-is-visible.html"
