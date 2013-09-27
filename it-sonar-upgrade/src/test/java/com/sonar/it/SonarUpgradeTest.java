7b79b5b320339c68cbbcf781e539908de00fe3be
Simon Brandhof <simon.brandhof@gmail.com>
2013-09-27 10:17:03 +0200

    SONAR-4596 support the fact that Java plugins are not installed on upgrades

--- it-sonar-upgrade/src/test/java/com/sonar/it/SonarUpgradeTest.java
+++ it-sonar-upgrade/src/test/java/com/sonar/it/SonarUpgradeTest.java
@@ -123,6 +123,10 @@ public class SonarUpgradeTest {
     if (!keepDatabase) {
       builder.restoreProfileAtStartup(FileLocation.ofClasspath("/sonar-way-2.7.xml"));
     }
+    if (VersionUtils.isGreaterThanOrEqual(version, "4.0")) {
+      // Java plugins are not installed on upgrades, but only on fresh installs
+      builder.addPlugin("java");
+    }
     orchestrator = builder.build();
     orchestrator.start();
   }
