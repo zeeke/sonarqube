d15fbbde7248d37baef7604c538cb98a9f65bd6e
Julien Lancelot <julien.lancelot@sonarsource.com>
2013-10-31 13:33:21 +0100

    Add IT on changelog using WS

--- it-sonar/tests/src/test/java/com/sonar/it/issue/suite/IssueChangelogTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/issue/suite/IssueChangelogTest.java
@@ -34,7 +34,7 @@ public class IssueChangelogTest extends AbstractIssueTestCase {
   }
 
   @Test
-  public void update_changelog_on_assign() throws Exception {
+  public void update_changelog_when_assigning_issue_by_user() throws Exception {
     assertThat(issueClient().changes(issue.key())).isEmpty();
     adminIssueClient().assign(issue.key(), "admin");
 
@@ -49,6 +49,52 @@ public class IssueChangelogTest extends AbstractIssueTestCase {
     assertThat(changeDiff.key()).isEqualTo("assignee");
     assertThat(changeDiff.oldValue()).isNull();
     assertThat(changeDiff.newValue()).isEqualTo("Administrator");
+  }
+
+  @Test
+  public void update_changelog_when_reopening_unresolved_issue_by_scan() throws Exception {
+    assertThat(issueClient().changes(issue.key())).isEmpty();
+
+    // re analyse the project after resolving an issue in order to reopen it
+    adminIssueClient().doTransition(issue.key(), "resolve");
+    orchestrator.executeBuild(scan);
+
+    List<IssueChange> changes = issueClient().changes(issue.key());
+    assertThat(changes).hasSize(2);
+
+    // Change done by the user (first change is be the oldest one)
+    IssueChange change1 = changes.get(0);
+    assertThat(change1.user()).isEqualTo("admin");
+    assertThat(change1.createdAt()).isNotNull();
+    assertThat(change1.updatedAt()).isNotNull();
+    assertThat(change1.diffs()).hasSize(2);
+
+    IssueChangeDiff change1Diff1 = change1.diffs().get(0);
+    assertThat(change1Diff1.key()).isEqualTo("resolution");
+    assertThat(change1Diff1.oldValue()).isNull();
+    assertThat(change1Diff1.newValue()).isEqualTo("FIXED");
+
+    IssueChangeDiff change1Diff2 = change1.diffs().get(1);
+    assertThat(change1Diff2.key()).isEqualTo("status");
+    assertThat(change1Diff2.oldValue()).isEqualTo("OPEN");
+    assertThat(change1Diff2.newValue()).isEqualTo("RESOLVED");
+
+    // Change done by scan
+    IssueChange change2 = changes.get(1);
+    assertThat(change2.user()).isNull();
+    assertThat(change2.createdAt()).isNotNull();
+    assertThat(change2.updatedAt()).isNotNull();
+    assertThat(change2.diffs()).hasSize(2);
+
+    IssueChangeDiff changeDiff1 = change2.diffs().get(0);
+    assertThat(changeDiff1.key()).isEqualTo("resolution");
+    assertThat(changeDiff1.oldValue()).isNull();
+    assertThat(changeDiff1.newValue()).isNull();
+
+    IssueChangeDiff changeDiff2 = change2.diffs().get(1);
+    assertThat(changeDiff2.key()).isEqualTo("status");
+    assertThat(changeDiff2.oldValue()).isEqualTo("RESOLVED");
+    assertThat(changeDiff2.newValue()).isEqualTo("REOPENED");
   }
 
   /**
