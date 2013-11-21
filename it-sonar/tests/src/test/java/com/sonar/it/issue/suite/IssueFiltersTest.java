e608f007dcc20f3765e33d299ee04bc77854b851
Simon Brandhof <simon.brandhof@gmail.com>
2013-11-21 12:09:11 +0100

    Temporarily disable test

--- it-sonar/tests/src/test/java/com/sonar/it/issue/suite/IssueFiltersTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/issue/suite/IssueFiltersTest.java
@@ -45,9 +45,11 @@ public class IssueFiltersTest extends AbstractIssueTestCase {
 
       // TODO temporarily disabled
       //"/selenium/issue/issue-filters/should-save-issue-filters.html",
+      //"/selenium/issue/issue-filters/should-not-save-filter-with-already-used-name.html"
+
       "/selenium/issue/issue-filters/should-not-save-issue-filter-if-no-name.html",
-      "/selenium/issue/issue-filters/should-save-issue-filters-with-description.html",
-      "/selenium/issue/issue-filters/should-not-save-filter-with-already-used-name.html"
+      "/selenium/issue/issue-filters/should-save-issue-filters-with-description.html"
+
     ).build());
   }
 
