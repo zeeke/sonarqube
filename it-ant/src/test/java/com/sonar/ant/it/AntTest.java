14339144ab9aab9e507c80fb6ce25081115b3abf
Julien HENRY <julien.henry@sonarsource.com>
2014-01-08 09:53:24 +0100

    Fix Ant ITs after release of Sonar Java 2.0 (1.5 is no more in update center)

--- it-ant/src/test/java/com/sonar/ant/it/AntTest.java
+++ it-ant/src/test/java/com/sonar/ant/it/AntTest.java
@@ -50,11 +50,13 @@ public class AntTest {
     // SONAR-4358
     // Wating for ORCH-184
     if (Version.create(builder.getSonarVersion()).isGreaterThanOrEquals("3.7")) {
-      // Update to Sonar Java 1.5 in order to allow installation of Cobertura 1.4
+      // Update to Sonar Java 2.0 in order to allow installation of Cobertura 1.4
       builder.removeDistributedPlugins()
-        .setOrchestratorProperty("javaVersion", "1.5")
+        .setOrchestratorProperty("javaVersion", "2.0")
         .addPlugin("java")
-        .addPlugin(MavenLocation.create("org.codehaus.sonar-plugins", "sonar-cobertura-plugin", "1.4"));
+        .addPlugin(MavenLocation.create("org.codehaus.sonar-plugins", "sonar-cobertura-plugin", "1.4"))
+        // PMD is used by testJavaVersion
+        .addPlugin(MavenLocation.create("org.codehaus.sonar-plugins.java", "sonar-pmd-plugin", "2.0"));
     }
 
     orchestrator = builder.build();
