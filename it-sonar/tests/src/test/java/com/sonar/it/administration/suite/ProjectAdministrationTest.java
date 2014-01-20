2a3e896439985c943ae117caae6741e642819b22
Julien HENRY <julien.henry@sonarsource.com>
2014-01-20 15:39:50 +0100

    Update resource keys

--- it-sonar/tests/src/test/java/com/sonar/it/administration/suite/ProjectAdministrationTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/administration/suite/ProjectAdministrationTest.java
@@ -34,7 +34,7 @@ public class ProjectAdministrationTest {
   public static Orchestrator orchestrator = AdministrationTestSuite.ORCHESTRATOR;
 
   private static final String PROJECT_KEY = "sample";
-  private static final String FILE_KEY = "sample:sample.Sample";
+  private static final String FILE_KEY = "sample:src/main/java/sample/Sample.java";
 
   @Before
   public void deleteAnalysisData() throws SQLException {
@@ -82,8 +82,8 @@ public class ProjectAdministrationTest {
   public void test_project_deletion() throws Exception {
     // For an unknown reason, this test fails if the analysis id one with SonarRunner...
     MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("shared/sample"))
-        .setCleanSonarGoals()
-        .setProperty("sonar.dynamicAnalysis", "false");
+      .setCleanSonarGoals()
+      .setProperty("sonar.dynamicAnalysis", "false");
     orchestrator.executeBuild(build.setProperty("sonar.projectDate", "2012-01-01"));
 
     Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("project-deletion", "/selenium/administration/project-deletion/project-deletion.html").build();
@@ -95,25 +95,25 @@ public class ProjectAdministrationTest {
     GregorianCalendar today = new GregorianCalendar();
 
     MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("shared/sample"))
-        .setCleanSonarGoals()
-        .setProperty("sonar.dynamicAnalysis", "false");
+      .setCleanSonarGoals()
+      .setProperty("sonar.dynamicAnalysis", "false");
     orchestrator.executeBuild(build.setProperty("sonar.projectDate", (today.get(Calendar.YEAR) - 1) + "-01-01"));
     // The analysis must be run once again to have an history so that it is possible to delete a snapshot
     orchestrator.executeBuild(build.setProperty("sonar.projectDate", (today.get(Calendar.YEAR)) + "-01-01"));
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("project-administration",
-            "/selenium/administration/project-administration/project-exclusions.html",
-            "/selenium/administration/project-administration/project-general-exclusions.html",
-            "/selenium/administration/project-administration/project-test-exclusions.html",
-            "/selenium/administration/project-administration/project-general-test-exclusions.html",
-            "/selenium/administration/project-administration/project-links.html",
-            "/selenium/administration/project-administration/project-modify-versions.html",
-            "/selenium/administration/project-administration/project-rename-current-version.html",
-            "/selenium/administration/project-administration/project-history-deletion.html", // SONAR-3206
-            "/selenium/administration/project-administration/project-quality-profile.html" // SONAR-3517
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("project-administration",
+        "/selenium/administration/project-administration/project-exclusions.html",
+        "/selenium/administration/project-administration/project-general-exclusions.html",
+        "/selenium/administration/project-administration/project-test-exclusions.html",
+        "/selenium/administration/project-administration/project-general-test-exclusions.html",
+        "/selenium/administration/project-administration/project-links.html",
+        "/selenium/administration/project-administration/project-modify-versions.html",
+        "/selenium/administration/project-administration/project-rename-current-version.html",
+        "/selenium/administration/project-administration/project-history-deletion.html", // SONAR-3206
+        "/selenium/administration/project-administration/project-quality-profile.html" // SONAR-3517
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -122,8 +122,8 @@ public class ProjectAdministrationTest {
   public void should_delete_version_of_multimodule_project() throws Exception {
     GregorianCalendar today = new GregorianCalendar();
     SonarRunner build = SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-multi-modules-sample"))
-        .setProperty("sonar.dynamicAnalysis", "false")
-        .setProperty("sonar.projectDate", (today.get(Calendar.YEAR) - 1) + "-01-01");
+      .setProperty("sonar.dynamicAnalysis", "false")
+      .setProperty("sonar.projectDate", (today.get(Calendar.YEAR) - 1) + "-01-01");
     orchestrator.executeBuild(build);
 
     // The analysis must be run once again to have an history so that it is possible
@@ -135,19 +135,19 @@ public class ProjectAdministrationTest {
     assertThat(count("events where category='Version'")).as("Different number of events").isEqualTo(7);
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("delete_version_of_multimodule_project",
-            "/selenium/administration/project-administration/multimodule-project-modify-version.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("delete_version_of_multimodule_project",
+        "/selenium/administration/project-administration/multimodule-project-modify-version.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
 
     assertThat(count("events where category='Version'")).as("Different number of events").isEqualTo(14);
 
     selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("delete_version_of_multimodule_project",
-            "/selenium/administration/project-administration/multimodule-project-delete-version.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("delete_version_of_multimodule_project",
+        "/selenium/administration/project-administration/multimodule-project-delete-version.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
 
     assertThat(count("events where category='Version'")).as("Different number of events").isEqualTo(7);
@@ -164,10 +164,10 @@ public class ProjectAdministrationTest {
     scanSample("2012-01-02", "alert-profile");
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("display-alerts-history-page",
-            "/selenium/administration/display-alerts-history-page/should-display-alerts-correctly-history-page.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("display-alerts-history-page",
+        "/selenium/administration/display-alerts-history-page/should-display-alerts-correctly-history-page.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -183,10 +183,10 @@ public class ProjectAdministrationTest {
     scanSampleWithProfile("alert-profile");
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("display-period-alerts",
-            "/selenium/administration/display-alerts/should-display-period-alerts-correctly.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("display-period-alerts",
+        "/selenium/administration/display-alerts/should-display-period-alerts-correctly.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -198,15 +198,15 @@ public class ProjectAdministrationTest {
     scanSampleWithDate("2012-01-01");
 
     Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("project-settings",
-        // SONAR-3425
-        "/selenium/administration/project-settings/override-global-settings.html",
+      // SONAR-3425
+      "/selenium/administration/project-settings/override-global-settings.html",
 
-        "/selenium/administration/project-settings/only-on-project-settings.html"
-        ).build();
+      "/selenium/administration/project-settings/only-on-project-settings.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
 
     assertThat(orchestrator.getServer().getAdminWsClient().find(PropertyQuery.createForResource("sonar.skippedModules", "sample")).getValue())
-        .isEqualTo("my-excluded-module");
+      .isEqualTo("my-excluded-module");
   }
 
   /**
@@ -215,20 +215,20 @@ public class ProjectAdministrationTest {
   @Test
   public void should_bulk_update_project_keys() {
     MavenBuild build = MavenBuild.builder()
-        .setPom(ItUtils.locateProjectPom("shared/multi-modules-sample"))
-        .addSonarGoal()
-        .withDynamicAnalysis(false)
-        .build();
+      .setPom(ItUtils.locateProjectPom("shared/multi-modules-sample"))
+      .addSonarGoal()
+      .withDynamicAnalysis(false)
+      .build();
     orchestrator.executeBuild(build);
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("project-bulk-update-keys",
-            "/selenium/administration/project-update-keys/bulk-update-impossible-because-duplicate-keys.html",
-            "/selenium/administration/project-update-keys/bulk-update-impossible-because-no-input.html",
-            "/selenium/administration/project-update-keys/bulk-update-impossible-because-no-match.html",
-            "/selenium/administration/project-update-keys/bulk-update-success.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("project-bulk-update-keys",
+        "/selenium/administration/project-update-keys/bulk-update-impossible-because-duplicate-keys.html",
+        "/selenium/administration/project-update-keys/bulk-update-impossible-because-no-input.html",
+        "/selenium/administration/project-update-keys/bulk-update-impossible-because-no-match.html",
+        "/selenium/administration/project-update-keys/bulk-update-success.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -238,18 +238,18 @@ public class ProjectAdministrationTest {
   @Test
   public void should_fine_grain_update_project_keys() {
     MavenBuild build = MavenBuild.builder()
-        .setPom(ItUtils.locateProjectPom("shared/multi-modules-sample"))
-        .addSonarGoal()
-        .withDynamicAnalysis(false)
-        .build();
+      .setPom(ItUtils.locateProjectPom("shared/multi-modules-sample"))
+      .addSonarGoal()
+      .withDynamicAnalysis(false)
+      .build();
     orchestrator.executeBuild(build);
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("project-fine-grained-update-keys",
-            "/selenium/administration/project-update-keys/fine-grained-update-impossible.html",
-            "/selenium/administration/project-update-keys/fine-grained-update-success.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("project-fine-grained-update-keys",
+        "/selenium/administration/project-update-keys/fine-grained-update-impossible.html",
+        "/selenium/administration/project-update-keys/fine-grained-update-success.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -264,11 +264,11 @@ public class ProjectAdministrationTest {
     scanSample();
 
     Selenese selenese = Selenese
-        .builder()
-        .setHtmlTestsInClasspath("manage-permissions",
-            "/selenium/administration/manage_project_roles/change_roles_of_users.html",
-            "/selenium/administration/manage_project_roles/change_roles_of_groups.html"
-        ).build();
+      .builder()
+      .setHtmlTestsInClasspath("manage-permissions",
+        "/selenium/administration/manage_project_roles/change_roles_of_users.html",
+        "/selenium/administration/manage_project_roles/change_roles_of_groups.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -282,17 +282,17 @@ public class ProjectAdministrationTest {
     scanSample();
 
     Selenese selenese = Selenese.builder()
-        .setHtmlTestsInClasspath("do_not_reset_default_roles_1",
-            "/selenium/administration/do_not_reset_default_roles/1_set_project_roles.html"
-        ).build();
+      .setHtmlTestsInClasspath("do_not_reset_default_roles_1",
+        "/selenium/administration/do_not_reset_default_roles/1_set_project_roles.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
 
     scanSample();
 
     selenese = Selenese.builder()
-        .setHtmlTestsInClasspath("do_not_reset_default_roles_2",
-            "/selenium/administration/do_not_reset_default_roles/2_project_roles_are_unchanged.html"
-        ).build();
+      .setHtmlTestsInClasspath("do_not_reset_default_roles_2",
+        "/selenium/administration/do_not_reset_default_roles/2_project_roles_are_unchanged.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -301,9 +301,9 @@ public class ProjectAdministrationTest {
     scanSample();
 
     Selenese selenese = Selenese.builder()
-        .setHtmlTestsInClasspath("anonymous_should_have_user_role_to_access_project",
-            "/selenium/administration/anonymous_should_have_user_role_to_access_project/remove_user_role.html"
-        ).build();
+      .setHtmlTestsInClasspath("anonymous_should_have_user_role_to_access_project",
+        "/selenium/administration/anonymous_should_have_user_role_to_access_project/remove_user_role.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
@@ -313,19 +313,19 @@ public class ProjectAdministrationTest {
   @Test
   public void should_display_module_settings() {
     orchestrator.executeBuild(MavenBuild.create(ItUtils.locateProjectPom("maven/modules-declaration"))
-        .setCleanSonarGoals()
-        .setProperty("sonar.dynamicAnalysis", "false"));
+      .setCleanSonarGoals()
+      .setProperty("sonar.dynamicAnalysis", "false"));
 
     Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("module-settings",
-        // SONAR-3425
-        "/selenium/administration/module-settings/display-module-settings.html"
-        ).build();
+      // SONAR-3425
+      "/selenium/administration/module-settings/display-module-settings.html"
+      ).build();
     orchestrator.executeSelenese(selenese);
   }
 
   private void scanSample(@Nullable String date, @Nullable String profile) {
     SonarRunner scan = SonarRunner.create(ItUtils.locateProjectDir("shared/sample"))
-        .setProperties("sonar.cpd.skip", "true");
+      .setProperties("sonar.cpd.skip", "true");
     if (date != null) {
       scan.setProperty("sonar.projectDate", date);
     }
