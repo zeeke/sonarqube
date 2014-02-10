22a4f5118e7ebf4f409e4f8c8a74c8ed0e5cb804
Simon Brandhof <simon.brandhof@gmail.com>
2014-02-10 19:54:42 +0100

    Projects have no language anymore

--- it-sonar/tests/src/test/java/com/sonar/it/cartography/ResourcesTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/cartography/ResourcesTest.java
@@ -7,9 +7,7 @@ package com.sonar.it.cartography;
 
 import com.sonar.it.ItUtils;
 import com.sonar.orchestrator.Orchestrator;
-import com.sonar.orchestrator.build.MavenBuild;
 import com.sonar.orchestrator.build.SonarRunner;
-import com.sonar.orchestrator.locator.MavenLocation;
 import org.junit.After;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -24,7 +22,6 @@ public class ResourcesTest {
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
-      .addPlugin(MavenLocation.create("org.codehaus.sonar-plugins.javascript", "sonar-javascript-plugin", "1.1"))
       .addPlugin(ItUtils.xooPlugin())
       .build();
 
@@ -33,32 +30,15 @@ public class ResourcesTest {
     orchestrator.getDatabase().truncateInspectionTables();
   }
 
+  /**
+   * SONAR-4235
+   */
   @Test
-  public void should_resources_be_filtered_by_language() {
-    scanProject("javascript/javascript-maven-project");
-    scanProject("shared/sample");
-
-    assertThat(orchestrator.getServer().getWsClient().findAll(new ResourceQuery())).hasSize(2);
-    assertThat(orchestrator.getServer().getWsClient().findAll(new ResourceQuery().setLanguages("java"))).hasSize(1);
-    assertThat(orchestrator.getServer().getWsClient().findAll(new ResourceQuery().setLanguages("js"))).hasSize(1);
-    assertThat(orchestrator.getServer().getWsClient().findAll(new ResourceQuery().setLanguages("java,js"))).hasSize(2);
-    assertThat(orchestrator.getServer().getWsClient().findAll(new ResourceQuery().setLanguages("unknown"))).isEmpty();
-  }
-
-  // SONAR-4235
-  @Test
-  public void test_resource_creation_date() {
+  public void test_project_creation_date() {
     long before = new Date().getTime();
     orchestrator.executeBuild(SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample")));
     long after = new Date().getTime();
-    Resource xooSample = orchestrator.getServer().getWsClient().find(new ResourceQuery().setLanguages("xoo"));
+    Resource xooSample = orchestrator.getServer().getWsClient().find(new ResourceQuery().setResourceKeyOrId("sample"));
     assertThat(xooSample.getCreationDate().getTime()).isGreaterThan(before).isLessThan(after);
-  }
-
-  private void scanProject(String project) {
-    MavenBuild maven = MavenBuild.create(ItUtils.locateProjectPom(project))
-        .setCleanSonarGoals()
-        .setProperty("sonar.dynamicAnalysis", "false");
-    orchestrator.executeBuild(maven);
   }
 }
