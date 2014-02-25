bb823da01fcc32054df1fb45e19e6d910090c71a
Simon Brandhof <simon.brandhof@gmail.com>
2014-02-25 09:52:31 +0100

    Enable CustomMetricTest

--- it-sonar/plugins/custom-metric-plugin/src/main/java/CustomSensor.java
+++ it-sonar/plugins/custom-metric-plugin/src/main/java/CustomSensor.java
@@ -3,12 +3,10 @@ import org.sonar.api.batch.Sensor;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.config.Settings;
 import org.sonar.api.measures.Measure;
-import org.sonar.api.resources.JavaFile;
+import org.sonar.api.resources.File;
 import org.sonar.api.resources.Project;
 
 public class CustomSensor implements Sensor {
-
-  private static final String TOO_LONG = StringUtils.repeat("0123456789", 401);
 
   private Settings settings;
 
@@ -21,10 +19,13 @@ public class CustomSensor implements Sensor {
   }
 
   public void analyse(Project project, SensorContext sensorContext) {
-    sensorContext.saveMeasure(JavaFile.create("/src/main/java/One.java"), CustomMetrics.CUSTOM, 1.0);
-    sensorContext.saveMeasure(JavaFile.create("/src/main/java/Two.java"), CustomMetrics.CUSTOM, 2.0);
     if (settings.getBoolean("sonar.it.failingMeasure")) {
-      sensorContext.saveMeasure(JavaFile.create("/src/main/java/Break.java", "Break.java", false), new Measure(CustomMetrics.CUSTOM).setValue(1.0).setAlertText(TOO_LONG));
+      // field is too long for database column -> raise error
+      sensorContext.saveMeasure(File.create("src/main/java/One.java"),
+        new Measure(CustomMetrics.CUSTOM).setValue(1.0).setAlertText(StringUtils.repeat("0123456789", 1000)));
+    } else {
+      sensorContext.saveMeasure(File.create("src/main/java/One.java"), CustomMetrics.CUSTOM, 1.0);
+      sensorContext.saveMeasure(File.create("src/main/java/Two.java"), CustomMetrics.CUSTOM, 2.0);
     }
   }
 }
