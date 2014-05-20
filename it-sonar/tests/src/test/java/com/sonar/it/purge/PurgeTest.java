d5113c04dda3e0e59d3d3bcee628aa41f5c3cc3b
Julien HENRY <julien.henry@sonarsource.com>
2014-05-20 15:45:18 +0200

    Fix PurgeIT after introduction of new profiles metric

--- it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
@@ -49,8 +49,8 @@ public class PurgeTest {
     assertThat(count("projects where qualifier in ('FIL')")).as("Wrong number of files").isEqualTo(320);
     assertThat(count("projects where qualifier in ('UTS')")).as("Wrong number of unit test files").isEqualTo(28);
 
-    int measuresOnTrk = 196;
-    int measuresOnBrc = 453;
+    int measuresOnTrk = 197;
+    int measuresOnBrc = 456;
     int measuresOnDir = 2664;
     int measuresOnFil = 12853;
 
@@ -81,8 +81,8 @@ public class PurgeTest {
     // must be a different date, else a single snapshot is kept per day
     scan("shared/struts-1.3.9-diet", DateFormatUtils.ISO_DATE_FORMAT.format(today));
 
-    int newMeasuresOnTrk = 140;
-    int newMeasuresOnBrc = 353;
+    int newMeasuresOnTrk = 141;
+    int newMeasuresOnBrc = 356;
     int newMeasuresOnDir = 594;
     int newMeasuresOnFil = 0;
 
