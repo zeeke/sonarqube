1a99815fb71c5c31e0d5df7f2811e70df35d24b9
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-06-18 09:39:20 +0200

    Improve test on manual rule

--- it-sonar/tests/src/test/java/com/sonar/it/rule/suite/ManualRulesTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/rule/suite/ManualRulesTest.java
@@ -7,13 +7,29 @@ package com.sonar.it.rule.suite;
 
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.selenium.Selenese;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ManualRulesTest {
 
   @ClassRule
   public static Orchestrator orchestrator = RuleTestSuite.ORCHESTRATOR;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    orchestrator.getDatabase().truncateInspectionTables();
+    deleteManualRules();
+  }
+
+  @AfterClass
+  public static void purgeManualRules() {
+    deleteManualRules();
+  }
 
   @Test
   public void testManualRules() {
@@ -23,6 +39,15 @@ public class ManualRulesTest {
         "/selenium/rule/manual-rules/create_edit_delete_manual_rule.html"
       ).build();
     orchestrator.executeSelenese(selenese);
+  }
+
+  protected static void deleteManualRules(){
+    try {
+      Connection connection = orchestrator.getDatabase().openConnection();
+      connection.prepareStatement("DELETE FROM rules WHERE rules.plugin_name='manual'").execute();
+    } catch (SQLException e) {
+      throw new IllegalStateException("Fail to remove manual rules", e);
+    }
   }
 
 }
