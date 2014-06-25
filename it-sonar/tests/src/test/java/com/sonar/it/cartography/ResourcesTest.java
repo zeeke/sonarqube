f7c22562312d41751cfd670976eeff52fbb2ad87
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-06-25 09:52:30 +0200

    Remove the category "cartography"

--- it-sonar/tests/src/test/java/com/sonar/it/cartography/ResourcesTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/cartography/ResourcesTest.java
@@ -5,40 +5,12 @@
  */
 package com.sonar.it.cartography;
 
-import com.sonar.it.ItUtils;
-import com.sonar.orchestrator.Orchestrator;
-import com.sonar.orchestrator.build.SonarRunner;
-import org.junit.After;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.sonar.wsclient.services.Resource;
-import org.sonar.wsclient.services.ResourceQuery;
-
-import java.util.Date;
-
-import static org.fest.assertions.Assertions.assertThat;
 
 public class ResourcesTest {
 
-  @ClassRule
-  public static Orchestrator orchestrator = Orchestrator.builderEnv()
-      .addPlugin(ItUtils.xooPlugin())
-      .build();
-
-  @After
-  public void cleanup() {
-    orchestrator.getDatabase().truncateInspectionTables();
-  }
-
-  /**
-   * SONAR-4235
-   */
   @Test
-  public void test_project_creation_date() {
-    long before = new Date().getTime();
-    orchestrator.executeBuild(SonarRunner.create(ItUtils.locateProjectDir("shared/xoo-sample")));
-    long after = new Date().getTime();
-    Resource xooSample = orchestrator.getServer().getWsClient().find(new ResourceQuery().setResourceKeyOrId("sample"));
-    assertThat(xooSample.getCreationDate().getTime()).isGreaterThan(before).isLessThan(after);
+  public void to_be_removed() {
+
   }
 }
