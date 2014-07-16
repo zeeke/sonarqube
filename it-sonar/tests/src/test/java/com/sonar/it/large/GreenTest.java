fa592d120756d09db5c7f0704e4bb07fe5c1ad2e
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-07-16 16:36:38 +0200

    Add empty large category

---
+++ it-sonar/tests/src/test/java/com/sonar/it/large/GreenTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.it.large;
+
+import org.junit.Test;
+
+public class GreenTest {
+  @Test
+  public void green() throws Exception {
+    // to be removed
+  }
+}
