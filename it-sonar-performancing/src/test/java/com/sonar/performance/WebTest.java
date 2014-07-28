c26833eaecbf19db38cb2573d89ec4e937f3d8d1
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-07-28 13:33:32 +0200

    Get the minimal duration of 10 HTTP requests

--- it-sonar-performancing/src/test/java/com/sonar/performance/WebTest.java
+++ it-sonar-performancing/src/test/java/com/sonar/performance/WebTest.java
@@ -24,7 +24,6 @@ public class WebTest extends PerfTestCase {
     .restoreProfileAtStartup(FileLocation.ofClasspath("/sonar-way-3.6.xml"))
     .build();
 
-
   @BeforeClass
   public static void scan_struts() throws Exception {
     FileLocation strutsHome = FileLocation.ofShared("it-sonar-performancing/struts-1.3.9/pom.xml");
@@ -122,22 +121,31 @@ public class WebTest extends PerfTestCase {
   PageStats request(String path) {
     String url = orchestrator.getServer().getUrl() + path;
 
-    // warm server
+    // warm-up
     for (int i = 0; i < 5; i++) {
       newRequest(url).code();
     }
 
-    HttpRequest request = newRequest(url);
-    long start = System.currentTimeMillis();
-    if (request.ok()) {
-      long duration = System.currentTimeMillis() - start;
-      int size = request.body().length();
-      PageStats counters = new PageStats(duration, size);
-      System.out.printf("##### Page %50s %7d ms %7d bytes\n", path, counters.durationMs, counters.sizeBytes);
-      return counters;
+    long targetDuration = Long.MAX_VALUE;
+    long targetSize = 0L;
+    for (int i = 0; i < 10; i++) {
+      HttpRequest request = newRequest(url);
+      long start = System.currentTimeMillis();
+      if (request.ok()) {
+        long duration = System.currentTimeMillis() - start;
+        int size = request.body().length();
+
+        if (duration < targetDuration) {
+          targetDuration = duration;
+          targetSize = size;
+        }
+        System.out.printf("##### Page %50s %7d ms %7d bytes\n", path, duration, size);
+      }
     }
-    fail(String.format("Failed to load page: %s (%d)", url, request.code()));
-    return new PageStats(Long.MAX_VALUE, Long.MAX_VALUE);
+    if (targetDuration == Long.MAX_VALUE) {
+      fail(String.format("Failed to load page: %s", url));
+    }
+    return new PageStats(targetDuration, targetSize);
   }
 
   private HttpRequest newRequest(String url) {
