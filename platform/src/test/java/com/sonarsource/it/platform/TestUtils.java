bc3df62d29af0c94892c81975b995b7f1fcdd898
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-08-28 12:21:55 +0200

    Temporarily deactivate objc license

--- platform/src/test/java/com/sonarsource/it/platform/TestUtils.java
+++ platform/src/test/java/com/sonarsource/it/platform/TestUtils.java
@@ -58,7 +58,6 @@ public class TestUtils {
       .activateLicense("cpp")
       .activateLicense("devcockpit")
       .activateLicense("natural")
-      .activateLicense("objc")
       .activateLicense("pacbase")
       .activateLicense("pli")
       .activateLicense("plsql")
