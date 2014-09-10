b5b08e96c4b3e3453e47d1642ba5752ce91978f7
Julien HENRY <julien.henry@sonarsource.com>
2014-09-10 14:41:04 +0200

    Fix dependency IT (make assertion independent of resource ids)

--- it-sonar/tests/src/test/java/com/sonar/it/batch/suite/DependencyTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/batch/suite/DependencyTest.java
@@ -42,10 +42,10 @@ public class DependencyTest {
     String jsonDeps = orchestrator.getServer().adminWsClient().post("/api/dependency_tree", "resource", "com.sonarsource.it.samples:sample-with-deps", "format", "json");
     JSONAssert
       .assertEquals(
-        "[{\"did\":\"5\",\"rid\":\"11\",\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"2.4\",\"k\":\"commons-io:commons-io\",\"n\":\"commons-io:commons-io\"},"
-          + "{\"did\":\"7\",\"rid\":\"12\",\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"4.10\",\"k\":\"junit:junit\",\"n\":\"junit:junit\",\"to\":["
-          + "{\"did\":\"6\",\"rid\":\"13\",\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1\",\"k\":\"org.hamcrest:hamcrest-core\",\"n\":\"org.hamcrest:hamcrest-core\"}]}]",
-        jsonDeps, true);
+        "[{\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"2.4\",\"k\":\"commons-io:commons-io\",\"n\":\"commons-io:commons-io\"},"
+          + "{\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"4.10\",\"k\":\"junit:junit\",\"n\":\"junit:junit\",\"to\":["
+          + "{\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1\",\"k\":\"org.hamcrest:hamcrest-core\",\"n\":\"org.hamcrest:hamcrest-core\"}]}]",
+        jsonDeps, false);
   }
 
   @Test
@@ -58,29 +58,27 @@ public class DependencyTest {
     String jsonDeps = orchestrator.getServer().adminWsClient().post("/api/dependency_tree", "resource", "com.sonarsource.it.samples:module_b", "format", "json");
     JSONAssert
       .assertEquals(
-        "[{\"did\":\"2\",\"rid\":\"2\",\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"BRC\",\"v\":\"1.0-SNAPSHOT\",\"k\":\"com.sonarsource.it.samples:module_a\",\"n\":\"Module A\"},"
-          + "{\"did\":\"3\",\"rid\":\"6\",\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"3.8.1\",\"k\":\"junit:junit\",\"n\":\"junit:junit\"}]",
-        jsonDeps, true);
+        "[{\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"BRC\",\"v\":\"1.0-SNAPSHOT\",\"k\":\"com.sonarsource.it.samples:module_a\",\"n\":\"Module A\"},"
+          + "{\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"3.8.1\",\"k\":\"junit:junit\",\"n\":\"junit:junit\"}]",
+        jsonDeps, false);
   }
 
   // SONAR-5604
   @Test
   public void inject_project_libraries_using_property() throws JSONException {
-    MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("shared/sample"))
-      .setCleanPackageSonarGoals()
-      .setProperty("sonar.maven.projectDependencies", "[{\"k\":\"antlr:antlr\",\"v\":\"2.7.2\",\"s\":\"compile\",\"d\":[]},"
-        + "{\"k\":\"commons-fileupload:commons-fileupload\",\"v\":\"1.1.1\",\"s\":\"compile\",\"d\":[{\"k\":\"commons-io:commons-io\",\"v\":\"1.1\",\"s\":\"compile\",\"d\":[]}]},"
-        + "{\"k\":\"junit:junit\",\"v\":\"3.8.1\",\"s\":\"test\",\"d\":[]}]");
+    // sonar.maven.projectDependencies is overriden in the pom.xml
+    MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("batch/dependencies/sample-with-deps-as-prop"))
+      .setCleanPackageSonarGoals();
     orchestrator.executeBuild(build);
 
     String jsonDeps = orchestrator.getServer().adminWsClient().post("/api/dependency_tree", "resource", "com.sonarsource.it.samples:simple-sample", "format", "json");
     JSONAssert
       .assertEquals(
-        "[{\"did\":\"1\",\"rid\":\"4\",\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"2.7.2\",\"k\":\"antlr:antlr\",\"n\":\"antlr:antlr\"},"
-          + "{\"did\":\"2\",\"rid\":\"5\",\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1.1\",\"k\":\"commons-fileupload:commons-fileupload\",\"n\":\"commons-fileupload:commons-fileupload\",\"to\":["
-          + "{\"did\":\"3\",\"rid\":\"6\",\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1\",\"k\":\"commons-io:commons-io\",\"n\":\"commons-io:commons-io\"}]},"
-          + "{\"did\":\"4\",\"rid\":\"7\",\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"3.8.1\",\"k\":\"junit:junit\",\"n\":\"junit:junit\"}]",
-        jsonDeps, true);
+        "[{\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"2.7.2\",\"k\":\"antlr:antlr\",\"n\":\"antlr:antlr\"},"
+          + "{\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1.1\",\"k\":\"commons-fileupload:commons-fileupload\",\"n\":\"commons-fileupload:commons-fileupload\",\"to\":["
+          + "{\"w\":1,\"u\":\"compile\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"1.1\",\"k\":\"commons-io:commons-io\",\"n\":\"commons-io:commons-io\"}]},"
+          + "{\"w\":1,\"u\":\"test\",\"s\":\"PRJ\",\"q\":\"LIB\",\"v\":\"3.8.1\",\"k\":\"junit:junit\",\"n\":\"junit:junit\"}]",
+        jsonDeps, false);
   }
 
 }
