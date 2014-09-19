9930fea549f159740627f7f5056ae191daf69115
Julien HENRY <julien.henry@sonarsource.com>
2014-09-19 11:21:59 +0200

    Fix Purge IT

--- it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
@@ -51,7 +51,7 @@ public class PurgeTest {
 
     int measuresOnTrk = 182;
     int measuresOnBrc = 413;
-    int measuresOnDir = 2460;
+    int measuresOnDir = 2514;
     int measuresOnFil = 10668;
 
     // count measures 
@@ -69,7 +69,7 @@ public class PurgeTest {
 
     int expectedMeasures = measuresOnTrk + measuresOnBrc + measuresOnDir + measuresOnFil;
     assertThat(count("project_measures")).as("Wrong number of measures").isEqualTo(expectedMeasures);
-    assertThat(count("project_measures where measure_data is not null")).as("Wrong number of measure data").isEqualTo(57);
+    assertThat(count("project_measures where measure_data is not null")).as("Wrong number of measure data").isEqualTo(56);
 
     // count other tables that are constant between 2 scans
     int expectedIssues = 3859;
@@ -105,7 +105,7 @@ public class PurgeTest {
     assertThat(count("project_measures")).as("Wrong number of measures after second analysis").isEqualTo(expectedMeasures);
 
     assertThat(count("snapshot_sources")).as("Wrong number of snapshot_sources").isEqualTo(expectedSources);
-    assertThat(count("project_measures where measure_data is not null")).as("Wrong number of measure data").isEqualTo(57);
+    assertThat(count("project_measures where measure_data is not null")).as("Wrong number of measure data").isEqualTo(56);
     assertThat(count("issues")).as("Wrong number of issues").isEqualTo(expectedIssues);
     assertThat(count("dependencies")).as("Wrong number of dependencies").isEqualTo(expectedDependencies);
   }
