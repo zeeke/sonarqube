6e1ec95cd5e484833c03e60481104657149e322b
Julien HENRY <julien.henry@sonarsource.com>
2014-10-29 15:21:13 +0100

    Add performnce test for Issue tracking

--- it-sonar-performancing/src/test/java/com/sonar/performance/batch/ScanTest.java
+++ it-sonar-performancing/src/test/java/com/sonar/performance/batch/ScanTest.java
@@ -5,27 +5,41 @@
  */
 package com.sonar.performance.batch;
 
-import com.sonar.performance.PerfTestCase;
-
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.orchestrator.locator.FileLocation;
 import com.sonar.orchestrator.locator.MavenLocation;
+import com.sonar.performance.PerfTestCase;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.sonar.wsclient.issue.BulkChangeQuery;
+import org.sonar.wsclient.issue.Issue;
+import org.sonar.wsclient.issue.IssueQuery;
+import org.sonar.wsclient.issue.Issues;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Properties;
+import java.util.Set;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ScanTest extends PerfTestCase {
 
-  @Rule
-  public TemporaryFolder temp = new TemporaryFolder();
+  private static final int NB_LINES_PER_FILE_BIG_PROJECT = 1000;
+
+  private static final int NB_FILES_BIG_PROJECT = 100;
+
+  @ClassRule
+  public static TemporaryFolder temp = new TemporaryFolder();
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
@@ -34,7 +48,7 @@ public class ScanTest extends PerfTestCase {
     .build();
 
   @BeforeClass
-  public static void setUp() {
+  public static void setUp() throws IOException {
     // Execute this query in order for next analysis to not freeze (FIXME why does it freeze?)
     orchestrator.resetData();
 
@@ -59,6 +73,95 @@ public class ScanTest extends PerfTestCase {
     orchestrator.executeBuild(runner);
     long duration = System.currentTimeMillis() - start;
     assertDurationAround(duration, 9700L);
+  }
+
+  @Test
+  public void scan_100000_issues_tracking() throws InvalidPropertiesFormatException, IOException {
+    File projectBaseDir = createBigXooProject(NB_FILES_BIG_PROJECT, NB_LINES_PER_FILE_BIG_PROJECT);
+    SonarRunner runner = newSonarRunner(
+      "-Xmx512m -server -XX:MaxPermSize=64m",
+      "sonar.profile", "one-xoo-issue-per-line",
+      "sonar.showProfiling", "true",
+      "sonar.scm.disabled", "true",
+      "sonar.projectKey", "foo",
+      "sonar.projectName", "Foo",
+      "sonar.projectVersion", "1.0",
+      "sonar.sources", "src"
+      ).setProjectDir(projectBaseDir);
+    orchestrator.executeBuild(runner);
+    Properties prof = readProfiling(projectBaseDir, "foo");
+    assertDurationAround(Long.valueOf(prof.getProperty("IssueTrackingDecorator")), 1645L);
+
+    // Second run
+    orchestrator.executeBuild(runner);
+    prof = readProfiling(projectBaseDir, "foo");
+    assertDurationAround(Long.valueOf(prof.getProperty("InitialOpenIssuesSensor")), 47000L);
+    assertDurationAround(Long.valueOf(prof.getProperty("IssueTrackingDecorator")), 6900L);
+  }
+
+  @Test
+  public void scan_500_issues_with_changelog_tracking() throws InvalidPropertiesFormatException, IOException {
+    int nbFiles = 10;
+    int nbIssuesPerFile = 50;
+    File projectBaseDir = createBigXooProject(nbFiles, nbIssuesPerFile);
+    SonarRunner runner = newSonarRunner(
+      "-Xmx512m -server -XX:MaxPermSize=64m",
+      "sonar.profile", "one-xoo-issue-per-line",
+      "sonar.showProfiling", "true",
+      "sonar.scm.disabled", "true",
+      "sonar.projectKey", "foo",
+      "sonar.projectName", "Foo",
+      "sonar.projectVersion", "1.0",
+      "sonar.sources", "src"
+      ).setProjectDir(projectBaseDir);
+    orchestrator.executeBuild(runner);
+    Properties prof = readProfiling(projectBaseDir, "foo");
+
+    Set<String> issueKeys = new HashSet<String>();
+    int count = nbFiles * nbIssuesPerFile;
+    int pageSize = 500;
+    for (int i = 1; i <= count / pageSize; i++) {
+      Issues issues = orchestrator.getServer().adminWsClient().issueClient().find(IssueQuery.create().componentRoots("foo").pageSize(pageSize).pageIndex(i));
+      for (Issue issue : issues.list()) {
+        issueKeys.add(issue.key());
+      }
+    }
+    assertThat(issueKeys).hasSize(count);
+
+    long start = System.currentTimeMillis();
+    // Create some changelog per issue
+    for (int i = 1; i <= 2; i++) {
+      orchestrator.getServer().adminWsClient().issueClient().bulkChange(BulkChangeQuery.create()
+        .issues(issueKeys.toArray(new String[pageSize]))
+        .actions("assign", "set_severity", "do_transition")
+        .actionParameter("assign", "assignee", "admin")
+        .actionParameter("do_transition", "transition", "confirm")
+        .actionParameter("set_severity", "severity", "MINOR"));
+      orchestrator.getServer().adminWsClient().issueClient().bulkChange(BulkChangeQuery.create()
+        .issues(issueKeys.toArray(new String[pageSize]))
+        .actions("assign", "set_severity", "do_transition")
+        .actionParameter("assign", "assignee", "")
+        .actionParameter("do_transition", "transition", "unconfirm")
+        .actionParameter("set_severity", "severity", "BLOCKER"));
+    }
+    System.out.println("Creating changelog took: " + (System.currentTimeMillis() - start) + " ms");
+
+    // Second run
+    orchestrator.executeBuild(runner);
+    prof = readProfiling(projectBaseDir, "foo");
+    assertDurationAround(Long.valueOf(prof.getProperty("InitialOpenIssuesSensor")), 24000L);
+  }
+
+  private Properties readProfiling(File baseDir, String moduleKey) throws FileNotFoundException, IOException, InvalidPropertiesFormatException {
+    File profiling = new File(baseDir, ".sonar/profiling/" + moduleKey + "-profiler.xml");
+    Properties props = new Properties();
+    FileInputStream in = new FileInputStream(profiling);
+    try {
+      props.loadFromXML(in);
+    } finally {
+      in.close();
+    }
+    return props;
   }
 
   @Test
@@ -133,6 +236,20 @@ public class ScanTest extends PerfTestCase {
       .setEnvironmentVariable("SONAR_RUNNER_OPTS", sonarRunnerOpts)
       .setRunnerVersion("2.3")
       .setProjectDir(FileLocation.of("projects/xoo-sample").getFile());
+  }
+
+  private static File createBigXooProject(int nbFiles, int nbIssuesPerFile) throws IOException {
+    File baseDir = temp.newFolder();
+
+    File src = new File(baseDir, "src");
+    for (int i = 1; i <= nbFiles; i++) {
+      File xooFile = new File(src, "file" + i + ".xoo");
+      FileUtils.write(xooFile, "Sample content");
+      File xooMeasureFile = new File(src, "file" + i + ".xoo.measures");
+      FileUtils.write(xooMeasureFile, "lines:" + nbIssuesPerFile);
+    }
+
+    return baseDir;
   }
 
 }
