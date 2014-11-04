a31eccad42e8cb2c9473293c14594eee0c65ea35
Julien HENRY <julien.henry@sonarsource.com>
2014-11-04 23:00:25 +0100

    Add performance tests for projects with lots of modules

--- it-sonar-performancing/src/test/java/com/sonar/performance/batch/suite/BootstrappingTest.java
+++ it-sonar-performancing/src/test/java/com/sonar/performance/batch/suite/BootstrappingTest.java
@@ -61,8 +61,8 @@ public class BootstrappingTest extends PerfTestCase {
 
     SonarRunner runner = SonarRunner.create()
       .setProperties(
-        "sonar.projectKey", "many-modules",
-        "sonar.projectName", "Many Modules",
+        "sonar.projectKey", "many-flat-modules",
+        "sonar.projectName", "Many Flat Modules",
         "sonar.projectVersion", "1.0",
         "sonar.sources", "",
         "sonar.showProfiling", "true")
@@ -73,6 +73,56 @@ public class BootstrappingTest extends PerfTestCase {
     long start = System.currentTimeMillis();
     orchestrator.executeBuild(runner);
     long duration = System.currentTimeMillis() - start;
+    // First analysis
+    assertDurationAround(duration, 4000L);
+
+    start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    duration = System.currentTimeMillis() - start;
+    // Second analysis
+    assertDurationAround(duration, 4000L);
+  }
+
+  @Test
+  public void analyzeProjectWith100NestedModules() throws IOException {
+    int SIZE = 100;
+
+    File baseDir = temp.newFolder();
+    File currentDir = baseDir;
+
+    for (int i = 1; i <= SIZE; i++) {
+      File projectProps = new File(currentDir, "sonar-project.properties");
+      FileUtils.write(projectProps, "sonar.modules=module" + i + "\n", true);
+      if (i >= 1) {
+        FileUtils.write(projectProps, "sonar.moduleKey=module" + (i - 1), true);
+      }
+      File moduleDir = new File(currentDir, "module" + i);
+      moduleDir.mkdir();
+      currentDir = moduleDir;
+    }
+    FileUtils.write(new File(currentDir, "sonar-project.properties"), "sonar.moduleKey=module" + SIZE, true);
+
+    SonarRunner runner = SonarRunner.create()
+      .setProperties(
+        "sonar.projectKey", "many-nested-modules",
+        "sonar.projectName", "Many Nested Modules",
+        "sonar.projectVersion", "1.0",
+        "sonar.sources", "",
+        "sonar.showProfiling", "true")
+      .setEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx512m -server -XX:MaxPermSize=64m")
+      .setRunnerVersion("2.4")
+      .setProjectDir(baseDir);
+
+    long start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    long duration = System.currentTimeMillis() - start;
+    // First analysis
+    assertDurationAround(duration, 4000L);
+
+    start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    duration = System.currentTimeMillis() - start;
+    // Second analysis
     assertDurationAround(duration, 4000L);
   }
 
--- it-sonar-performancing/src/test/java/com/sonar/performance/batch/suite/MemoryTest.java
+++ it-sonar-performancing/src/test/java/com/sonar/performance/batch/suite/MemoryTest.java
@@ -5,13 +5,24 @@
  */
 package com.sonar.performance.batch.suite;
 
+import com.google.common.base.Strings;
 import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.performance.PerfTestCase;
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.sonar.wsclient.services.PropertyCreateQuery;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MemoryTest extends PerfTestCase {
+
+  @ClassRule
+  public static TemporaryFolder temp = new TemporaryFolder();
 
   @ClassRule
   public static Orchestrator orchestrator = BatchPerfTestSuite.ORCHESTRATOR;
@@ -26,6 +37,57 @@ public class MemoryTest extends PerfTestCase {
     orchestrator.executeBuild(
       newSonarRunner("-Xmx80m -server -XX:-HeapDumpOnOutOfMemoryError")
       );
+  }
+
+  int DEPTH = 6;
+
+  @Test
+  public void analyzeProjectWithManyModulesAndBigProperties() throws IOException {
+
+    File baseDir = temp.newFolder();
+
+    prepareModule(baseDir, "moduleA", 1);
+    prepareModule(baseDir, "moduleB", 1);
+    prepareModule(baseDir, "moduleC", 1);
+
+    FileUtils.write(new File(baseDir, "sonar-project.properties"), "sonar.modules=moduleA,moduleB,moduleC\n", true);
+    FileUtils.write(new File(baseDir, "sonar-project.properties"), "sonar.myBigProp=" + Strings.repeat("A", 1000), true);
+
+    SonarRunner runner = SonarRunner.create()
+      .setProperties(
+        "sonar.projectKey", "big-module-tree",
+        "sonar.projectName", "Big Module Tree",
+        "sonar.projectVersion", "1.0",
+        "sonar.sources", "",
+        "sonar.showProfiling", "true")
+      .setEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx512m -server -XX:MaxPermSize=64m")
+      .setRunnerVersion("2.4")
+      .setProjectDir(baseDir);
+
+    long start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    long duration = System.currentTimeMillis() - start;
+    assertDurationAround(duration, 4000L);
+
+    // Second execution with a property on server side
+    orchestrator.getServer().getAdminWsClient().create(new PropertyCreateQuery("sonar.anotherBigProp", Strings.repeat("B", 1000), "big-module-tree"));
+    start = System.currentTimeMillis();
+    orchestrator.executeBuild(runner);
+    duration = System.currentTimeMillis() - start;
+    assertDurationAround(duration, 4000L);
+  }
+
+  private void prepareModule(File parentDir, String moduleName, int depth) throws IOException {
+    File moduleDir = new File(parentDir, moduleName);
+    moduleDir.mkdir();
+    File projectProps = new File(moduleDir, "sonar-project.properties");
+    FileUtils.write(projectProps, "sonar.moduleKey=" + moduleName + "\n", true);
+    if (depth < DEPTH) {
+      FileUtils.write(projectProps, "sonar.modules=" + moduleName + "A," + moduleName + "B," + moduleName + "C\n", true);
+      prepareModule(moduleDir, moduleName + "A", depth + 1);
+      prepareModule(moduleDir, moduleName + "B", depth + 1);
+      prepareModule(moduleDir, moduleName + "C", depth + 1);
+    }
   }
 
 }
