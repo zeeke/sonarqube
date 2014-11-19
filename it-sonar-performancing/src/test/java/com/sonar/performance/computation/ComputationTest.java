66370baf6a14a306fa500734092ffd7f5398ed5c
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-11-19 14:09:10 +0100

    Add skeleton for computation performance tests

---
+++ it-sonar-performancing/src/test/java/com/sonar/performance/computation/ComputationTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2013-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.performance.computation;
+
+import org.junit.Test;
+
+public class ComputationTest {
+
+  @Test
+  public void fake() throws Exception {
+
+  }
+}
