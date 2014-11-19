07e0259b6b26127b3bc13bf8516f46b364637da0
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-11-19 14:16:59 +0100

    Add skeleton for computation integration tests

---
+++ it-sonar/tests/src/test/java/com/sonar/it/computation/ComputationTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2009-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
+package com.sonar.it.computation;
+
+import org.junit.Test;
+
+public class ComputationTest {
+
+  @Test
+  public void fake() throws Exception {
+    // up to you Teryk
+  }
+}
