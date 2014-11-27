23a3ac467e6f9ffa5c443bce4908b4989c81c727
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2014-11-27 18:16:39 +0100

    purge large test relies on xoo project

--- it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/purge/PurgeTest.java
@@ -7,9 +7,9 @@ package com.sonar.it.purge;
 
 import com.sonar.it.ItUtils;
 import com.sonar.orchestrator.Orchestrator;
-import com.sonar.orchestrator.build.MavenBuild;
+import com.sonar.orchestrator.build.BuildResult;
+import com.sonar.orchestrator.build.SonarRunner;
 import com.sonar.orchestrator.locator.FileLocation;
-import com.sonar.orchestrator.locator.MavenLocation;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.junit.Before;
@@ -34,15 +34,16 @@ public class PurgeTest {
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
-    .restoreProfileAtStartup(FileLocation.ofClasspath("/sonar-way-2.7.xml"))
-    .addPlugin(MavenLocation.of("org.codehaus.sonar-plugins.java", "sonar-checkstyle-plugin", "2.1.1"))
-    .addPlugin(MavenLocation.of("org.codehaus.sonar-plugins.java", "sonar-pmd-plugin", "2.1"))
+    .addPlugin(ItUtils.xooPlugin())
+    .removeDistributedPlugins()
+    .setContext("/")
     .build();
 
   @Before
   public void deleteProjectData() {
     orchestrator.resetData();
 
+    orchestrator.getServer().restoreProfile(FileLocation.ofClasspath("/xoo/one-issue-per-line.xml"));
     orchestrator.getServer().getAdminWsClient().delete(
       new PropertyDeleteQuery("sonar.dbcleaner.hoursBeforeKeepingOnlyOneSnapshotByDay"));
   }
@@ -52,18 +53,18 @@ public class PurgeTest {
     Date today = new Date();
     Date yesterday = DateUtils.addDays(today, -1);
 
-    scan("shared/struts-1.3.9-diet", DateFormatUtils.ISO_DATE_FORMAT.format(yesterday));
+    scan("purge/xoo-multi-modules-sample", DateFormatUtils.ISO_DATE_FORMAT.format(yesterday));
 
     // count components
-    collector.checkThat("Wrong number of projects", count("projects where qualifier in ('TRK','BRC')"), equalTo(4));
-    collector.checkThat("Wrong number of directories", count("projects where qualifier in ('DIR')"), equalTo(40));
-    collector.checkThat("Wrong number of files", count("projects where qualifier in ('FIL')"), equalTo(320));
-    collector.checkThat("Wrong number of unit test files", count("projects where qualifier in ('UTS')"), equalTo(28));
+    collector.checkThat("Wrong number of projects", count("projects where qualifier in ('TRK','BRC')"), equalTo(7));
+    collector.checkThat("Wrong number of directories", count("projects where qualifier in ('DIR')"), equalTo(4));
+    collector.checkThat("Wrong number of files", count("projects where qualifier in ('FIL')"), equalTo(4));
+    collector.checkThat("Wrong number of unit test files", count("projects where qualifier in ('UTS')"), equalTo(0));
 
-    int measuresOnTrk = 182;
-    int measuresOnBrc = 413;
-    int measuresOnDir = 2491;
-    int measuresOnFil = 10666;
+    int measuresOnTrk = 50;
+    int measuresOnBrc = 264;
+    int measuresOnDir = 141;
+    int measuresOnFil = 69;
 
     // count measures 
     logMeasures("First analysis - TRK measures", "TRK");
@@ -81,21 +82,21 @@ public class PurgeTest {
 
     int expectedMeasures = measuresOnTrk + measuresOnBrc + measuresOnDir + measuresOnFil;
     collector.checkThat("Wrong number of measures", count("project_measures"), equalTo(expectedMeasures));
-    collector.checkThat("Wrong number of measure data", count("project_measures where measure_data is not null"), equalTo(56));
+    collector.checkThat("Wrong number of measure data", count("project_measures where measure_data is not null"), equalTo(0));
 
     // count other tables that are constant between 2 scans
-    int expectedIssues = 3859;
-    int expectedDependencies = 977;
+    int expectedIssues = 49;
+    int expectedDependencies = 0;
 
     collector.checkThat("Wrong number of issues", count("issues"), equalTo(expectedIssues));
     collector.checkThat("Wrong number of dependencies", count("dependencies"), equalTo(expectedDependencies));
 
     // must be a different date, else a single snapshot is kept per day
-    scan("shared/struts-1.3.9-diet", DateFormatUtils.ISO_DATE_FORMAT.format(today));
+    scan("purge/xoo-multi-modules-sample", DateFormatUtils.ISO_DATE_FORMAT.format(today));
 
-    int newMeasuresOnTrk = 130;
-    int newMeasuresOnBrc = 321;
-    int newMeasuresOnDir = 577;
+    int newMeasuresOnTrk = 54;
+    int newMeasuresOnBrc = 292;
+    int newMeasuresOnDir = 32;
     int newMeasuresOnFil = 0;
 
     logMeasures("Second analysis - TRK measures", "TRK");
@@ -109,12 +110,12 @@ public class PurgeTest {
     collector.checkThat(
       "Wrong number of measure of new_ metrics",
       count("project_measures, metrics where metrics.id = project_measures.metric_id and metrics.name like 'new_%'"),
-      equalTo(769));
+      equalTo(88));
 
     // added measures relate to project and new_* metrics
     expectedMeasures += newMeasuresOnTrk + newMeasuresOnBrc + newMeasuresOnDir + newMeasuresOnFil;
     collector.checkThat("Wrong number of measures after second analysis", count("project_measures"), equalTo(expectedMeasures));
-    collector.checkThat("Wrong number of measure data", count("project_measures where measure_data is not null"), equalTo(56));
+    collector.checkThat("Wrong number of measure data", count("project_measures where measure_data is not null"), equalTo(0));
     collector.checkThat("Wrong number of issues", count("issues"), equalTo(expectedIssues));
     collector.checkThat("Wrong number of dependencies", count("dependencies"), equalTo(expectedDependencies));
   }
@@ -126,11 +127,11 @@ public class PurgeTest {
   public void should_keep_all_snapshots_the_first_day() {
 
     // analyse once
-    scan("shared/sample");
+    scan("purge/xoo-multi-modules-sample");
     // analyse twice
-    scan("shared/sample");
+    scan("purge/xoo-multi-modules-sample");
     // and check we have 2 snapshots
-    assertThat(count("snapshots s where s.project_id=(select p.id from projects p where p.kee='com.sonarsource.it.samples:simple-sample')")).isEqualTo(2);
+    assertThat(count("snapshots s where s.project_id=(select p.id from projects p where p.kee='com.sonarsource.it.samples:multi-modules-sample')")).isEqualTo(2);
   }
 
   /**
@@ -139,14 +140,14 @@ public class PurgeTest {
   @Test
   public void should_keep_only_one_snapshot_per_day() {
 
-    scan("shared/struts-1.3.9-diet");
+    scan("purge/xoo-multi-modules-sample");
 
     int snapshotsCount = count("snapshots where qualifier<>'LIB'");
     int measuresCount = count("project_measures");
     // Using the "sonar.dbcleaner.hoursBeforeKeepingOnlyOneSnapshotByDay" property set to '0' is the way
     // to keep only 1 snapshot per day
     orchestrator.getServer().getAdminWsClient().update(new PropertyUpdateQuery("sonar.dbcleaner.hoursBeforeKeepingOnlyOneSnapshotByDay", "0"));
-    scan("shared/struts-1.3.9-diet");
+    scan("purge/xoo-multi-modules-sample");
     assertThat(count("snapshots where qualifier<>'LIB'")).as("Different number of snapshots").isEqualTo(snapshotsCount);
 
     int measureOnNewMetrics = count("project_measures, metrics where metrics.id = project_measures.metric_id and metrics.name like 'new_%'");
@@ -161,15 +162,15 @@ public class PurgeTest {
   public void should_delete_removed_modules() {
 
     scan("purge/modules/before");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:module_b");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:module_b1");
+    assertSingleSnapshot("com.sonarsource.it.samples:multi-modules-sample:module_b");
+    assertSingleSnapshot("com.sonarsource.it.samples:multi-modules-sample:module_b:module_b1");
 
     // we want the previous snapshot to be purged
     orchestrator.getServer().getAdminWsClient().update(new PropertyUpdateQuery("sonar.dbcleaner.hoursBeforeKeepingOnlyOneSnapshotByDay", "0"));
     scan("purge/modules/after");
-    assertDeleted("com.sonarsource.it.samples.purge:module_b");
-    assertDeleted("com.sonarsource.it.samples.purge:module_b1");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:module_c");
+    assertDeleted("com.sonarsource.it.samples:multi-modules-sample:module_b");
+    assertDeleted("com.sonarsource.it.samples:multi-modules-sample:module_b:module_b1");
+    assertSingleSnapshot("com.sonarsource.it.samples:multi-modules-sample:module_c:module_c1");
   }
 
   /**
@@ -179,13 +180,11 @@ public class PurgeTest {
   public void should_delete_removed_files() {
 
     scan("purge/files/before");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:files:src/main/java/sample/Sample.java");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:files:src/main/java/sample/Sample.java");
+    assertSingleSnapshot("com.sonarsource.it.samples:multi-modules-sample:module_a:module_a1:src/main/xoo/com/sonar/it/samples/modules/a1/HelloA1.xoo");
 
     scan("purge/files/after");
-    assertDeleted("com.sonarsource.it.samples.purge:files:src/main/java/sample/Sample.java");
-    assertDeleted("com.sonarsource.it.samples.purge:files:src/main/java/sample/Sample.java");
-    assertSingleSnapshot("com.sonarsource.it.samples.purge:files:src/main/java/sample/NewSample.java");
+    assertDeleted("src/main/xoo/com/sonar/it/samples/modules/a1/HelloA1.xoo");
+    assertSingleSnapshot("com.sonarsource.it.samples:multi-modules-sample:module_a:module_a1:src/main/xoo/com/sonar/it/samples/modules/a1/NewHelloA1.xoo");
   }
 
   /**
@@ -194,11 +193,11 @@ public class PurgeTest {
   @Test
   public void should_delete_historical_data_of_directories_by_default() {
 
-    scan("shared/sample", "2012-01-01");
+    scan("purge/xoo-multi-modules-sample", "2012-01-01");
     String select = "snapshots where scope='DIR'";
     int directorySnapshots = count(select);
 
-    scan("shared/sample", "2012-02-02");
+    scan("purge/xoo-multi-modules-sample", "2012-02-02");
     assertThat(count(select)).isEqualTo(directorySnapshots);
   }
 
@@ -207,19 +206,13 @@ public class PurgeTest {
    */
   @Test
   public void should_not_delete_historical_data_of_directories() {
-    scan("shared/sample", "2012-01-01");
+    scan("purge/xoo-multi-modules-sample", "2012-01-01");
 
     String select = "snapshots where scope='DIR'";
     int directorySnapshots = count(select);
 
     orchestrator.getServer().getAdminWsClient().update(new PropertyUpdateQuery("sonar.dbcleaner.cleanDirectory", "false"));
-    MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("shared/sample"))
-      .setCleanSonarGoals()
-      .setProperty("sonar.dynamicAnalysis", "false")
-      .setProfile("sonar-way-2.7")
-      .setProperty("sonar.projectDate", "2012-02-02");
-
-    orchestrator.executeBuild(build);
+    scan("purge/xoo-multi-modules-sample", "2012-02-02");
 
     assertThat(count(select)).isEqualTo(2 * directorySnapshots);
   }
@@ -230,7 +223,7 @@ public class PurgeTest {
   @Test
   public void should_delete_historical_data_of_flagged_metrics() {
 
-    scan("shared/sample", "2012-01-01");
+    scan("purge/xoo-multi-modules-sample", "2012-01-01");
 
     // historical data of complexity_in_classes is supposed to be deleted (see CoreMetrics)
     String selectNcloc = "project_measures where metric_id in (select id from metrics where name='ncloc')";
@@ -238,7 +231,7 @@ public class PurgeTest {
     int nclocCount = count(selectNcloc);
     int complexitInClassesCount = count(selectComplexityInClasses);
 
-    scan("shared/sample", "2012-02-02");
+    scan("purge/xoo-multi-modules-sample", "2012-02-02");
     assertThat(count(selectNcloc)).isGreaterThan(nclocCount);
     assertThat(count(selectComplexityInClasses)).isEqualTo(complexitInClassesCount);
   }
@@ -253,23 +246,24 @@ public class PurgeTest {
     assertThat(count("resource_index ri where ri.resource_id=(select p.id from projects p where p.kee='" + key + "')")).isGreaterThan(1);
   }
 
-  private MavenBuild scan(String path, String date) {
+  private BuildResult scan(String path, String date) {
     return scan(path, "sonar.projectDate", date);
   }
 
-  private MavenBuild scan(String path, String... extraProperties) {
-    MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom(path))
-      .setGoals("clean package", "sonar:sonar")
-      .setProperty("skipTests", "true")
-      .setProfile("sonar-way-2.7");
-    if (extraProperties != null) {
-      build.setProperties(extraProperties);
-    }
-    orchestrator.executeBuild(build);
-    return build;
+  private BuildResult scan(String path, String... extraProperties) {
+    SonarRunner runner = configureRunner(path, extraProperties);
+    return orchestrator.executeBuild(runner);
+  }
+
+  private SonarRunner configureRunner(String projectPath, String... props) {
+    SonarRunner runner = SonarRunner.create(ItUtils.locateProjectDir(projectPath))
+      .setProfile("one-issue-per-line")
+      .setProperties(props);
+    return runner;
   }
 
   private int count(String condition) {
+    System.out.print(orchestrator.getDatabase().executeSql("select * from " + condition));
     return orchestrator.getDatabase().countSql("select count(*) from " + condition);
   }
 
@@ -297,5 +291,4 @@ public class PurgeTest {
       System.out.println("  " + row);
     }
   }
-
 }
