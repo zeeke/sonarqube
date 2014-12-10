f559d138952d97a4899f7600a551d80c1ad18213
Julien HENRY <julien.henry@sonarsource.com>
2014-12-10 18:12:55 +0100

    Add missing license header

--- it-sonar-performancing/src/test/java/com/sonar/performance/PerfRule.java
+++ it-sonar-performancing/src/test/java/com/sonar/performance/PerfRule.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2013-2014 SonarSource SA
+ * All rights reserved
+ * mailto:contact AT sonarsource DOT com
+ */
 package com.sonar.performance;
 
 import com.google.common.base.Joiner;
