e200ad5a153726b459041d1e146c330a99c93576
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-01-12 12:00:15 +0100

    Revert "Temporary ignore failing test"
    This reverts commit 759b1c55d657830f199a35e636744b16a31982f9.

--- it-sonar/tests/src/test/java/com/sonar/it/batch/suite/DependencyTest.java
+++ it-sonar/tests/src/test/java/com/sonar/it/batch/suite/DependencyTest.java
@@ -9,7 +9,10 @@ import com.sonar.it.ItUtils;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.MavenBuild;
 import org.json.JSONException;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -62,7 +65,6 @@ public class DependencyTest {
 
   // SONAR-1587
   @Test
-  @Ignore
   public void multi_module_deps_with_branch() throws JSONException {
     // Module B depends on Module A
     MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom("batch/dependencies/multi-modules-with-deps"))
